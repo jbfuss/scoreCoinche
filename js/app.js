e751033f39b57b929eaf3c8bddefce63c0c9dd82
Jean-Baptiste FUSS <fuss.jeanbaptiste@gmail.com>
2016-08-09 20:21:21 +0200

    add panel

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 
-var myApp = angular.module('myApp',[]);//creating app
+var myApp = angular.module('myApp',["pageslide-directive"]);//creating app
 
 myApp.controller('myController', ['$scope', function($scope) { // creating controller
   $scope.newRoundOpen = false;
+  $scope.tableOpen = false;
 }]);
