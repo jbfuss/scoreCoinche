3862b7e2b2601001a3e8da4ca624f4d29d142852
unknown <a614808@FR-CNU1230G19.ww930.my-it-solutions.net>
2016-08-10 16:40:27 +0200

    Keyboard

--- js/app.js
+++ js/app.js
@@ -2,6 +2,35 @@
 var myApp = angular.module('myApp',["pageslide-directive"]);//creating app
 
 myApp.controller('myController', ['$scope', function($scope) { // creating controller
-  $scope.newRoundOpen = false;
-  $scope.tableOpen = false;
+  	$scope.newRoundOpen = false;
+  	$scope.tableOpen = false;
+  	$scope.scoreOpen=false;
+  	$scope.template = { name: 'template2.html', url: 'template/score.html'};
+	$scope.score = { value: '0'};
+
+	$scope.changeScoreValue = function(value) { 
+		if($scope.score.value=='0'){
+			$scope.score.value=""+value;
+		}else{
+			if(($scope.score.value+value)<=250){
+				$scope.score.value = $scope.score.value+""+value;
+			}
+		}
+	};
+
+	$scope.deleteScore = function() { 
+		if($scope.score.value!='0'){
+			$scope.score.value= $scope.score.value.substring(0, $scope.score.value.length - 1);
+			if($scope.score.value==''){
+				$scope.score.value='0';
+			}
+		}
+
+	};
+	$scope.changeScoreByCapot =  function() {$scope.score.value='250'};
+
+	$scope.validateScore=function(){
+		console.log('nous '+$scope.score.value);
+		console.log('eux '+Math.max(0,162-$scope.score.value));
+	}
 }]);
